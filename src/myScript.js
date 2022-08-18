c56d572412827d8a4bd1a77f6e3f5315b74015e2
reza ramezani <rezaramezanir1367@gmail.com>
2022-08-19 00:28:10 +0430

    second commit

--- src/myScript.js
+++ src/myScript.js
@@ -37,49 +37,48 @@ function checkWin(value, type) {
   y = value % 3;
 
   // for axis  x
-  let countX = 0;
+  let count = 0;
   for (let i = 0; i < 3; i++) {
     if (b[x][i] == target) {
-      countX += 1;
+      count += 1;
     }
   }
-  if (countX == 3) {
-    console.log("countX");
+  if (count == 3) {
     modal(type);
   }
   // for axis  y
-  let countY = 0;
+  count = 0;
   for (let j = 0; j < 3; j++) {
     if (b[j][y] == target) {
-      countY += 1;
+      count += 1;
     }
   }
-  if (countY == 3) {
-    console.log("countY");
+  if (count == 3) {
     modal(type);
   }
 
   // main diameter
-  let countM = 0;
+  count = 0;
   if (x == y) {
     for (let i = 0; i < 3; i++) {
       if (b[i][i] == target) {
-        countM += 1;
+        count += 1;
       }
     }
   }
-  if (countM == 3) {
-    console.log("countM");
+  if (count == 3) {
     modal(type);
   }
 
   // Subdiameter
-
-  if ((x == 0 && y == 2) || (x == 1 && y == 1) || (x == 2 && y == 0)) {
-    if (b[0][2] == target && b[1][1] == target && b[2][0] == target) {
-      console.log("Subdiameter");
-      modal(type);
+  count = 0;
+  for (let i = 2; i <= 6; i += 2) {
+    if (a[i] == target) {
+      count += 1;
     }
+  }
+  if (count == 3) {
+    modal(type);
   }
 }
 function checkComputer(value) {
@@ -99,62 +98,62 @@ function checkComputer(value) {
       y = value % 3;
       // for axis  x
       let selectComputer = -1;
-      let countX = 0;
+      let count = 0;
       for (let i = 0; i < 3; i++) {
-        console.log(b[x][i]);
+        // console.log(b[x][i]);
         if (b[x][i] == target) {
-          countX += 1;
+          count += 1;
         } else if (b[x][i] == 0) {
           selectComputer = x * 3 + i;
         }
       }
-      if (countX == 2) {
+      if (count == 2) {
         if (a[selectComputer] == 0) {
           randomComputer = zeroNumbers.findIndex((x) => x == selectComputer);
         }
       }
       // for axis  y
-      let countY = 0;
+      count = 0;
       for (let i = 0; i < 3; i++) {
-        console.log(b[i][y]);
+        // console.log(b[i][y]);
         if (b[i][y] == target) {
-          countY += 1;
+          count += 1;
         } else if (b[i][y] == 0) {
           selectComputer = i * 3 + y;
         }
       }
-      if (countY == 2) {
+      if (count == 2) {
         if (a[selectComputer] == 0) {
           randomComputer = zeroNumbers.findIndex((x) => x == selectComputer);
         }
       }
 
       // main diameter
-      let countM = 0;
+      count = 0;
       if (x == y) {
         for (let i = 0; i < 3; i++) {
           if (b[i][i] == target) {
-            countM += 1;
+            count += 1;
           } else if (b[i][i] == 0) {
             selectComputer = i * 3 + i;
           }
         }
       }
-      if (countM == 2) {
+      if (count == 2) {
         if (a[selectComputer] == 0) {
           randomComputer = zeroNumbers.findIndex((x) => x == selectComputer);
         }
       }
       // Subdiameter
-      let countS = 0;
+      count = 0;
       for (let i = 2; i <= 6; i += 2) {
         if (a[i] == target) {
-          countS += 1;
+          count += 1;
         } else if (a[i] == 0) {
           selectComputer = i;
         }
       }
-      if (countS == 2) {
+      if (count == 2) {
         if (a[selectComputer] == 0) {
           randomComputer = zeroNumbers.findIndex((x) => x == selectComputer);
         }
